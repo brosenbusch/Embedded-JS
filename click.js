617b48146258462b5425a0acaadd03fa95103dda
brosenbusch <8brosenbusch@stpatrick.org>
2017-03-21 13:59:11 -0500

    Add files via upload

---
+++ click.js
@@ -0,0 +1,22 @@
+//declare samecolor() here
+function samecolor(){
+  let doc = document;
+  let divs = doc.querySelectorAll("#samecolor div");
+
+  divs[0].style.backgroundColor = rgb(100,200,50);
+  divs[1].style.backgroundColor = rgb(100,200,50);
+  divs[2].style.backgroundColor = rgb(100,200,50);
+  divs[3].style.backgroundColor = rgb(100,200,50);
+
+  divs[0].style.opacity = 0.2;
+  divs[1].style.opacity = 0.4;
+  divs[2].style.opacity = 0.8;
+  divs[3].style.opacity = 1;
+}
+//declare diffcolor() here
+function diffcolor(){
+  let doc = document;
+  let divs2 = doc.querySelectorAll("#diffcolor div");
+
+  divs2.style.
+}
