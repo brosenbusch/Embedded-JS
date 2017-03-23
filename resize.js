1974789211ec5ca95e6c031207dd4b08bd0befa4
brosenbusch <8brosenbusch@stpatrick.org>
2017-03-23 13:57:39 -0500

    Add files via upload

--- resize.js
+++ resize.js
@@ -1,6 +1,28 @@
 //Declare tabletSize() here
-function tableSize(){
+function tabletSize(){
   let doc = document;
-  let all = doc.querySelectorAll("#body section");
+  let all = doc.querySelectorAll("section");
+  let width = all.style.width;
+  let height = all.style.height;
+
+  all[0].style.width = String(width*0.75)+"px";
+  all[0].style.height = String(height*0.75)+"px";
+  all[1].style.width = String(width*0.75)+"px";
+  all[1].style.height = String(height*0.75)+"px";
+
 }
 //Declare phoneSize() here
+function phoneSize(){
+  let doc = document;
+  let all = doc.querySelectorAll("section");
+  let width = section.width;
+  let height = section.height;
+
+  all[0].style.width = String(width*0.50)+"px";
+  all[0].style.height = String(height*0.50)+"px";
+  all[1].style.width = String(width*0.50)+"px";
+  all[1].style.height = String(height*0.50)+"px";
+
+
+
+}
